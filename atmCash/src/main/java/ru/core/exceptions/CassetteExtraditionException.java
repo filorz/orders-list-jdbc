3db3fd9433b80426bd70f8fc6db38097abad1da2
Filipp Bogdanov <eefilee@gmail.com>
2021-12-02 16:25:24 +0300

    Revert "[01.12.2021] - ДЗ по АТМ"
    This reverts commit d0352e62888ab17b90d8cab64da8eca39966e7bf.

--- atmCash/src/main/java/ru/core/exceptions/CassetteExtraditionException.java
+++
@@ -1,7 +0,0 @@
-package ru.core.exceptions;
-
-public class CassetteExtraditionException extends RuntimeException {
-    public CassetteExtraditionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
